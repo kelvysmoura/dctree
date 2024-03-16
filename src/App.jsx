6740ebc9338f6de650d3b243446501c263b22bc2
Kelvys Moura <kelvysmoura2@gmail.com>
2024-03-16 16:55:10 -0300

    Renderizando componente dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import { useState } from 'react';
+import './App.css';
+
+import Link from './components/Link';
+import LinkList from './components/LinkList';
+
+const links = [
+  {
+    label: "Quero aprender a programar"
+  },
+  {
+    label: "Quero aprender data analytics"
+  },
+  {
+    label: "Quero aprender marketing digital"
+  },
+  {
+    label: "Inscreva-se nos nossos cursos",
+    title: "Curso intensivo",
+    description: "Veja aqui quais os cursos intensivos estão com turmas abertas e garanta sua vaga."
+  },
+  {
+    label: "Clique e se inscreva",
+    title: "Digital Plus",
+    description: "Evento exclusivo para alunos!"
+  }
+];
 
 function App() {
-  return (<div>Hello World</div>);
+  return (
+    <LinkList>
+      { links.map(item => <Link {...item} />) }
+    </LinkList>
+  );
 }
 
 export default App;
