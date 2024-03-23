6137f47aa6e39b87a8860e81ed2e78acd5441431
Kelvys Moura <kelvysmoura2@gmail.com>
2024-03-23 16:53:38 -0300

    Criando routas

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    useParams
+} from "react-router-dom";
+
+import Home from '@/pages/Home';
+import AccountLinks from '@/pages/AccountLinks';
+import Dashboard from '@/pages/Admin/Dashboard';
+
+export default createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={ <Home /> }></Route>
+            <Route path="/:accountlink" element={ <AccountLinks /> }></Route>
+            <Route path="/admin">
+                <Route path="dashboard" element={ <Dashboard /> }></Route>
+            </Route>
+        </>
+    )
+)
