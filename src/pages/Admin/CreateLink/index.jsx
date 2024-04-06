e473b0cdba09011a23b110719b40aead1b3dc082
Kelvys Moura <kelvysmoura2@gmail.com>
2024-04-06 16:54:08 -0300

    Criando componente CreateLink

---
+++ src/pages/Admin/CreateLink/index.jsx
@@ -0,0 +1,11 @@
+
+import Admin from "@/pages/Admin";
+import Form from '@/components/Form';
+
+export default function CreateLink() {
+    return ( 
+        <Admin>
+            <Form />
+        </Admin>
+    );
+} 
